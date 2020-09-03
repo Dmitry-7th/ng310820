abd8a5cd8a25f51e88e316bc2f5fea29eca03691
Ihor <igornepipenko@gmail.com>
2020-09-03 21:37:33 +0300

    Feature: lecture 1 [ref:no-ref]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng310820';
+  imgWidth = 50;
+  public imgSrc = 'https://i7.pngflow.com/pngimage/14/568/png-angularjs-logo-javascript-security-token-angle-triangle-logo-computer-programming-clipart.png';
+  public content = this.domSanitizer.bypassSecurityTrustHtml('<span style="color:red">Angular</span>');
+
+  public text = '';
+
+
+  constructor(
+    private readonly domSanitizer: DomSanitizer
+  ) {
+  }
+
+  public clickOnImg(divEl: HTMLDivElement, _event: MouseEvent): void {
+    console.log(divEl);
+  }
+
+  public search(value: string): void {
+    this.text = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ExampleModule } from './example/example.module';
+import { FormsModule } from '@angular/forms';
+// NgModule => es6 module
+// declarations - es6 let/const
+// imports - es6 import
+// exports - es6 export
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExampleModule,
+    FormsModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
